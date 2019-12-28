1ca34bad53921dd55b36040bad3819609ed08722
Harvir Hansra <h.hansra97@gmail.com>
2019-12-28 18:13:21 +0000

    Emacs and tmux changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,20 +1,26 @@
-(menu-bar-mode -1)
-(setq inhibit-startup-message t)
-(global-linum-mode t) 
-(setq linum-format "%d ")
 
 ;; Enables basic packaging support
 (require 'package)
+
+;; Initializes the package infrastructure
+(package-initialize)
+
 
 ;; Adds the Melpa archive to the list of available repositories
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 
-;; Initializes the package infrastructure
-(package-initialize)
+;; Cleansup menus
+(menu-bar-mode -1)
+(setq inhibit-startup-message t)
+
+;; Line numbers
+(global-linum-mode t) 
+(setq linum-format "%d ")
 
 ;; Enable elpy
 (elpy-enable)
+(setq elpy-rpc-backend "jedi")
 
 ;; Enable autopep8
 (require 'py-autopep8)
@@ -24,6 +30,9 @@
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; Show brackets
+(add-hook 'after-init-hook'show-paran-mode)
 
 ;; If there are no archived package contents, refresh them
 (when (not package-archive-contents)
@@ -35,7 +44,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (find-file-in-project magit py-autopep8 flycheck elpy))))
+    (rtags find-file-in-project magit py-autopep8 flycheck elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
