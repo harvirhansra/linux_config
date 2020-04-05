3ef07317efe666b17e885d7d556d2ff13ad94f67
Harvir Hansra <h.hansra97@gmail.com>
2020-04-05 22:04:31 +0000

    updates

--- .bashrc
+++ .bashrc
@@ -1,5 +1,7 @@
 export DISPLAY=192.168.1.113:0.0
 
+. ~/venvs/default_python/bin/activate
+
 alias c="clear"
 alias gits="git status"
 alias e="emacsclient -t"
@@ -7,9 +9,11 @@ alias tmux="TERM=xterm-256color tmux"
 alias size="du -csh"
 alias sizedirs="du -h --max-depth=1 | sort -hr"
 alias gits="git status"
-
+alias python=python3.8
 
 export PS1="\[\033[01;32m\]\u\[\033[01;36m\]@\[\033[01;31m\]\h \[\033[01;36m\]( \[\033[01;35m\]\w \[\033[01;36m\])\[\033[01;32m\]: \[$txtcyn\]\$git_branch \[\033[00m\]"
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/:/usr/local/lib64/pkgconfig/
+export DISPLAY=176.250.202.9:0.0
 
 find_git_branch() {
     local branch
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,7 @@
-
 ;; Enables basic packaging support
 (require 'package)
+(setq package-archives '())
+(setq package-check-signature nil)
 
 ;; Initializes the package infrastructure
 (package-initialize)
@@ -9,14 +10,21 @@
 ;; Adds the Melpa archive to the list of available repositories
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("GNUelpa" . "http://elpa.gnu.org/packages/") t)
 
-;; Cleansup menus
+
+;; Cleansup menus and loads theme
 (menu-bar-mode -1)
+(load-theme 'spacemacs-dark t)
 (setq inhibit-startup-message t)
 
 ;; Line numbers
 (global-linum-mode t) 
 (setq linum-format "%d ")
+
 
 ;; Enable elpy
 (elpy-enable)
@@ -24,15 +32,17 @@
 
 ;; Enable autopep8
 (require 'py-autopep8)
+(setq py-autopep8-options '("--max-line-length=120"))
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+;; Highlights brackets
+(show-paren-mode 1)
 
 ;; Enable Flycheck
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
-;; Show brackets
-(add-hook 'after-init-hook'show-paran-mode)
 
 ;; If there are no archived package contents, refresh them
 (when (not package-archive-contents)
