2f41f611fb32a8f578c94127ab1d802a30240f82
Harvir Hansra <h.hansra97@gmail.com>
2022-04-16 13:47:21 +0100

    updates

--- .bashrc
+++ .bashrc
@@ -1,15 +1,17 @@
 alias c="clear"
 alias gits="git status"
 alias e="emacsclient -t"
+alias em="emacs -nw"
 alias tmux="TERM=xterm-256color tmux"
 alias size="du -csh"
 alias sizedirs="du -h --max-depth=1 | sort -hr"
-alias gits="git status"
+alias findi="find . -iname"
+alias grep="grep --color=auto"
+alias .1="cd .."
+alias .2="cd ../.."
+alias .3="cd ../../.."
+alias .4="cd ../../../.."
 
-. ~/venvs/backtesting/bin/activate
-
-export DISPLAY=localhost:10.0
-export BOOST_ROOT=/usr/local/boost_1_72_0
 export PS1="\[\033[01;32m\]\u\[\033[01;36m\]@\[\033[01;31m\]\h \[\033[01;36m\]( \[\033[01;35m\]\w \[\033[01;36m\])\[\033[01;32m\]: \[$txtcyn\]\$git_branch \[\033[00m\]"
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/:/usr/local/lib64/pkgconfig/
 
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,5 @@
 ;; Enables basic packaging support
 (require 'package)
-(setq package-archives '())
-(setq package-check-signature nil)
-
-;; Initializes the package infrastructure
-(package-initialize)
-
 
 ;; Adds the Melpa archive to the list of available repositories
 (add-to-list 'package-archives
@@ -16,45 +10,86 @@
              '("GNUelpa" . "http://elpa.gnu.org/packages/") t)
 
 
-;; Cleansup menus and loads theme
-(menu-bar-mode -1)
-(load-theme 'spacemacs-dark t)
-(setq inhibit-startup-message t)
+;; Initializes the package infrastructure
+(package-initialize)
 
-;; Line numbers
-(global-linum-mode t) 
-(setq linum-format "%d ")
+;; Nord theme
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+(load-theme nord)
 
+;; popwin
+(require 'popwin)
+(popwin-mode 1)
+(global-set-key (kbd "M-z") popwin:keymap)
 
-;; Enable elpy
-(elpy-enable)
-(setq elpy-rpc-backend "jedi")
+;; Smooth scrolling
+(require 'smooth-scrolling)
+(smooth-scrolling-mode 1)
 
-;; Enable autopep8
-(require 'py-autopep8)
-(setq py-autopep8-options '("--max-line-length=120"))
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+;; Direx
+(require 'direx)
+(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory)
+(push '(direx:direx-mode :position left :width 50 :dedicated t)
+      popwin:special-display-config)
+(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
 
-;; Highlights brackets
+;; linum
+(global-linum-mode t)
+(setq linum-format "%d  ")
+
+;; Company for autocomplete
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Highlight brackets
 (show-paren-mode 1)
 
-;; Enable Flycheck
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+;; Removes menu bar
+(menu-bar-mode -1)
+(setq inhibit-startup-message t)
 
+;; Helm
+(helm-mode)
+(require 'helm-xref)
+(define-key global-map [remap find-file] #'helm-find-files)
+(define-key global-map [remap execute-extended-command] #'helm-M-x)
+(define-key global-map [remap switch-to-buffer] #'helm-mini)
 
-;; If there are no archived package contents, refresh them
-(when (not package-archive-contents)
-  (package-refresh-contents))
+;; lsp-mode
+(which-key-mode)
+(add-hook 'rust-mode-hook 'lsp)
+(add-hook 'python-mode-hook 'lsp)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+(setq lsp-eldoc-render-all t)
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode))
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default)))
  '(package-selected-packages
    (quote
-    (rtags find-file-in-project magit py-autopep8 flycheck elpy))))
+    (rustic avy company dap-mode flycheck helm-lsp helm-xref hydra lsp-treemacs projectile smooth-scrolling which-key yasnippet lsp-mode nord-theme)))
+ '(rustic-cargo-bin "/home/harvir/.cargo/bin/cargo"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
